780bb1eb449535d9c3181e3a221fa9b9292d7419
bgrayd <bgrayd@gmail.com>
2014-05-16 13:09:15 -0500

    Created two separate folder
    The code for the robot is in the RobotCode folder and the code for the
    driver station is in the DriverStationCode folder.  minor changes were
    made to some of the files

--- RobotCode/ping_sensor.py
+++ RobotCode/ping_sensor.py
@@ -4,24 +4,24 @@ class ping():
     def __init__(port, sensors):
         self.port = port
         self.sensors = sensors
-		self.raw = -1
+	self.raw = -1
 
     def readFeet():
         #read it from the sensor port and convert to feet
-		self.update()
+	self.update()
         return self.getFeet()
 
     def readCm():
         #read distance from the sensor port and convert to feet
         self.update()
-		return self.getCm()
+	return self.getCm()
 
-	def getCm():
-		return self.raw
+    def getCm():
+	return self.raw
 
-	def getFeet():
-		return self.raw*2.54/12
+    def getFeet():
+	return self.raw*2.54/12
 
-	def update():
-		return self.raw = ultrasonicRead(self.port)
+    def update():
+	return self.raw = ultrasonicRead(self.port)
 
