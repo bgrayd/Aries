837d531dfc998adfd51674e04c3f5fa2a85a8706
David <dns105@msstate.edu>
2014-05-17 14:58:44 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import threading, subprocess, math
-import ping_sensor, motors
+import ping_sensor, motor
 
 #constants
 FRONTPING=0
@@ -45,22 +45,133 @@ def main():
 def external_updater(externals):
     #cycle through the externals updating them
     #some will be inputs and some will be outputs
-
+	pass
 
 
 def location_tracking(deviceDrivers):
-    #uses the accelerameter to keep track of its position
-
+    #uses the accelerometer to keep track of its position
+	pass
 
 
 def orientation():
     #minimize two adjacent ping sensors' distance
+    leftPing=deviceDrivers[LEFTPING]
+    rightPing=deviceDrivers[RIGHTPING]
+    frontPing=deviceDrivers[FRONTPING]
+    rearPing=deviceDrivers[REARPING]
+    leftWheel=deviceDrivers[LEFTWHEEL]
+    rightWheel=deviceDrivers[RIGHTWHEEL]
 
+    '''
+    for turning, basically use the left or right ping as a reference
+    point. The the robot turns until the front ping sensor distance is 
+    relatively close to where the left or right ping was at. 20 is just 
+    being used as an arbitrary value and will likely have to be changed.
+    '''
+    def turnLeft():
+        turnt = leftPing.readCm()
+        frontDist = 0
+        while frontDist > turnt + 20 or frontDist < turnt - 20:
+            rightWheel.writePercent(75)
+            leftWheel.writePercent(-75)
+            frontDist = frontPing.readCm()
 
+    def turnRight():
+        turnt = rightPing.readCm()
+        frontDist = 0
+        while frontDist > turnt + 20 or frontDist < turnt -20:
+            rightWheel.writePercent(-75)
+            leftWheel.writePercent(75)
+            frontDist = frontPing.readCm()
+
+    aligned = False
+    '''
+    not really sure how well this would work. any other suggestions on minimizing the distances with the pings? 
+    I think the delta variables were used properly
+    '''
+    while aligned != True:
+        rightWheel.writePercent(75)
+        leftWheel.writePercent(-75)
+        leftDist = leftPing.readCm()
+        rightDist = rightPing.readCm()
+        deltaLeft = math.fabs(leftDist-WHEELPINGNORMAL)
+        deltaRight = math.fabs(rightDist-WHEELPINGNORMAL)
+        if deltaLeft > 70 or deltaRight > 70:   #arbitrary value that indicates when robot is no longer facing a wall at an angle
+            aligned = True
+        else:
+            aligned = False
     #roughly center horizontally
+    distances = []
+    frontDist = frontPing.readCm()
+    rightDist = rightPing.readCm()
+    rearDist = rearPing.readCm()
+    leftDist = leftPing.readCm()
+    distances.append(frontDist)
+    distances.append(rightDist)
+    distances.append(rearDist)
+    distances.append(leftDist)
+    maxDist = max(distances)
 
-    #turn to face dig end
+    distDiff = 0   #indicates when robot is roughly centered on the field
+    if frontDist is maxDist:
+        if leftDist > rightDist:
+            turnLeft()
+            while distDiff > 30:  #arbitray, determine later
+                leftWheel.writePercent(75)
+                rightWheel.writePercent(75)
+                distDiff = fabs(frontPing.readCm()-backPing.readCm())
+            turnRight()
+        else:
+            turnRight()
+            while distDiff > 30:
+               leftWheel.writePercent(75)
+               rightWheel.writePercent(75)
+               distDiff = fabs(frontPing.readCm()-backPing.readCm())
+            turnLeft()
 
+    if rightDist is maxDist:
+        if frontDist > rearDist:
+            while distDiff > 30:
+                leftWheel.writePercent(75)
+                rightWheel.writePercent(75)
+                distDiff = fabs(frontPing.readCm()-backPing.readCm())
+            turnRight()
+        else:
+            while distDiff > 30:
+                leftWheel.writePercent(-75)
+                rightWheel.writePercent(-75)
+                distDiff = fabs(frontPing.readCm()-backPing.readCm())
+            turnRight()
+    
+    if rearDist is maxDist:
+        if rightDist > leftDist:
+            turnRight()
+            while distDiff > 30:
+                leftWheel.writePercent(75)
+                rightWheel.writePercent(75)
+                distDiff = fabs(frontPing.readCm()-backPing.readCm())
+            turnRight()
+        else:
+            turnRight()
+            while distDiff > 30:
+                leftWheel.writePercent(-75)
+                rightWheel.writePercent(-75)
+                distDiff = fabs(frontPing.readCm()-backPing.readCm())
+            turnRight()
+    
+    if leftDist is maxDist:
+        if frontDist > rearDist:
+            while distDiff > 30:
+                leftWheel.writePercent(75)
+                rightWheel.writePercent(75)
+                distDiff = fabs(frontPing.readCm()-backPing.readCm())
+            turnLeft()
+        else:
+            while distDiff > 30:
+                leftWheel.writePercent(-75)
+                rightWheel.writePercent(-75)
+                distDiff = fabs(frontPing.readCm()-backPing.readCm())
+            turnLeft()
 
     
 def toDigArea():
@@ -69,7 +180,7 @@ def toDigArea():
     rightPing=deviceDrivers[RIGHTWHEELPING]
     leftWheel=deviceDrivers[LEFTWHEEL]
     rightWheel=deviceDrivers[RIGHTWHEEL]
-    while(!inDigArea()):
+    while not inDigArea():
         leftDist = leftPing.readCm()
         rightDist = rightPing.readCm()
         deltaLeft = math.fabs(leftDist-WHEELPINGNORMAL)
@@ -81,12 +192,12 @@ def toDigArea():
         #debug
         if(deltaLeft < WHEELPINGDELTA):
             rightWheel.writePercent(75)
-        else
+        else:
             rightWheel.writePercent(25)
             
         if(deltaRight < WHEELPINGDELTA):
             leftWheel.writePercent(75)
-        else
+        else:
             leftWheel.writePercent(-25)
             
         
@@ -110,7 +221,7 @@ def Dig():
     while(True):
         #digging from navigation area to wall
         while(frontPing < DistanceFromWallFront):
-            leftWheel.writePercent(diForwardSpeed)
+            leftWheel.writePercent(digForwardSpeed)
             rightWheel.writePercent(digForwardSpeed)
             conveyor.writePercent(conveyorSpeed)
             if(isFull()):
@@ -126,7 +237,7 @@ def Dig():
         
         #digging from wall to navigation area
         while(backPing < DistanceAwayFromWallBack):
-            leftWheel.writePercent(diForwardSpeed)
+            leftWheel.writePercent(digForwardSpeed)
             rightWheel.writePercent(digForwardSpeed)
             conveyor.writePercent(conveyorSpeed)
             if(isFull()):
@@ -141,18 +252,19 @@ def Dig():
 
     
 def toDumpArea():
-
+	pass
     
 def dump():
-
+	pass
 
 #returns a bool
 def inDigArea():
     #use location tracking and ping sensors to tell if it is in the digging area
-
+	pass
 
 #returns boolean, if hopper is full
 def isFull():
+	pass
 
 
 main()
